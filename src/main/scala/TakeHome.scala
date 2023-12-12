19344de2c2456e1d5aea06a193b70d563426e782
Joshua <zgh.joshua@gmail.com>
2023-12-12 12:50:04 -0800

    Clean the code

--- src/main/scala/TakeHome.scala
+++ src/main/scala/TakeHome.scala
@@ -1,50 +1,51 @@
 package take_home
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
+
+import org.apache.log4j.{Logger, Level}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.DataFrame
-import plotly._
-import plotly.element._
-import plotly.layout._
-import plotly.Almond._
 import scala.collection.immutable.Seq
 import com.typesafe.config.ConfigFactory
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
+
 object TakeHome {
-  def main(args: Array[String]): Unit ={
-    // Load the configuration
+  def main(args: Array[String]): Unit = {
+    // Load configuration settings
     val config = ConfigFactory.load("application.conf")
     val datasetPath = config.getString("takehome.paths.dataset")
     val logLevel = config.getString("takehome.logging.level")
 
+    // Set log level for Spark and Akka to suppress verbose logs
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
-    val spark = SparkSession
-      .builder
+
+    // Initialize Spark Session
+    val spark = SparkSession.builder
       .master("local[*]")
       .appName("TakeHome")
       .getOrCreate()
     spark.sparkContext.setLogLevel(logLevel)
+
+    // Load and display raw data
     val rawData = Data_Extraction.loadFromCSV(datasetPath)
     println(rawData.printSchema())
     println(rawData.show(15))
     println(rawData.describe().show())
 
+    // Explore distinct values in each column
     rawData.columns.foreach { columnName =>
         val distinctCount = rawData.select(columnName).distinct().count()
         println(s"Column $columnName has $distinctCount distinct values")
     }
     
+    // Clean the data
     val cleanedData = Data_Processing.cleanData(rawData)
 
+    // Perform data aggregation
     Data_Aggregation.aggregateData(cleanedData)
 
+    // Conduct time series analysis
     Time_Series_Analysis.analyze(cleanedData)
-    
 
-  // Stop the Spark session
+    // Stop the Spark session
     spark.stop()
   }
 }
