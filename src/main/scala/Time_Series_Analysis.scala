e9b237b35bcab5477d7cab720ea59d0be341f58f
Joshua <zgh.joshua@gmail.com>
2023-12-12 12:40:20 -0800

    Update moving average in time series report

--- src/main/scala/Time_Series_Analysis.scala
+++ src/main/scala/Time_Series_Analysis.scala
@@ -1,75 +1,85 @@
 package take_home
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.Window
 import plotly._
 import plotly.element._
 import plotly.layout._
 import plotly.Almond._
 import scala.collection.immutable.Seq
 
-
 object Time_Series_Analysis {
+
+    // Function to plot a line chart.
     def plotLineChart(xValues: Seq[String], yValues: Seq[Double], title: String, outputFile: String): Unit = {
-            val timeSeriesPlot = Seq(
-                Scatter(
-                xValues,
-                yValues.map(_.toDouble),
-                mode = ScatterMode(ScatterMode.Lines)
-                )
-            )
+        val timeSeriesPlot = Seq(
+            Scatter(xValues, yValues, mode = ScatterMode(ScatterMode.Lines))
+        )
 
-            // Define the layout of the plot.
-            val layout = Layout(title)
+        val layout = Layout(title)
+        plotly.Plotly.plot(outputFile, timeSeriesPlot, layout)
+    }
 
-            // Render the plot to a file (HTML).
-            plotly.Plotly.plot(outputFile, timeSeriesPlot, layout)
-        }
-    def analyze(df: DataFrame): Any = {
-        // Total Impression per day
-        val impressionsByDay = df.groupBy("date")
-        .agg(sum("total_impressions").alias("daily_impressions"))
-        .orderBy("date")
-
-        val windowSpec = org.apache.spark.sql.expressions.Window.orderBy("date").rowsBetween(-7, 0) // 7-day moving average
+    def movingAvg(impressionsByDay: DataFrame): DataFrame = {
+        val windowSpec = Window.orderBy("date").rowsBetween(-7, 0)
         val movingAvg = impressionsByDay.withColumn("moving_avg", avg("daily_impressions").over(windowSpec))
-        println(movingAvg.show())
+        movingAvg.show(30)
+        val movingAvgPlt = movingAvg.collect()
+                                    .map(row => (row.getAs[java.sql.Date]("date").toString, 
+                                                row.getAs[Double]("moving_avg")))
 
-        // Reformat the daily impression
+        plotLineChart(movingAvgPlt.map(_._1).to[scala.collection.immutable.Seq], 
+                movingAvgPlt.map(_._2).to[scala.collection.immutable.Seq],
+            "7-day Moving Average of Daily Impressions Over Time", 
+            "reports/7_Day_MovAvg_Impression.html")
+        movingAvg
+    }
+
+    def dailyImpression(impressionsByDay: DataFrame): Unit = {
         val timeSeriesData = impressionsByDay
             .withColumn("date", to_date(col("date")))
             .collect()
             .map(row => (row.getAs[java.sql.Date]("date").toString, row.getAs[Long]("daily_impressions").toDouble))
-            .toSeq
-        // Convert to immutable Seq explicitly and ensure dates are converted to strings
-        val dateStrings = timeSeriesData.map(_._1).to[scala.collection.immutable.Seq]
-        val immutableImpressions = timeSeriesData.map(_._2).to[scala.collection.immutable.Seq]
 
-        plotLineChart(dateStrings, immutableImpressions, "Daily Impressions Over Time", "reports/impressions_time_series.html")
+        plotLineChart(timeSeriesData.map(_._1).to[scala.collection.immutable.Seq], 
+                      timeSeriesData.map(_._2).to[scala.collection.immutable.Seq],
+                    "Daily Impressions Over Time", 
+                    "reports/impressions_time_series.html")
 
+        val detailedDailyImpression = impressionsByDay
+            .withColumn("day_of_week", date_format(col("date"), "EEEE"))
+            .select("date", "day_of_week", "daily_impressions")
+        detailedDailyImpression.show(30)
+    }
 
-        // Convert the date column to Date type
-        val impressionsByDayWithDate = impressionsByDay.withColumn("date", to_date(col("date")))
-        val impressionsByDayWithDayOfWeek = impressionsByDayWithDate.withColumn("day_of_week", date_format(col("date"), "EEEE"))
-        // Collect the data and create a new DataFrame called detailed_daily_impression
-        val detailed_daily_impression = impressionsByDayWithDayOfWeek.select("date", "day_of_week", "daily_impressions")
-        println(detailed_daily_impression.show(30))
-
-
+    def dailyRevenue(df: DataFrame): Unit = {
         // Total revenue per day
         val revenueByDay = df.groupBy("date")
-        .agg(sum("total_revenue").alias("daily_revenue"))
-        .orderBy("date")
-        val timeSeriesData_rev = revenueByDay
+            .agg(sum("total_revenue").alias("daily_revenue"))
+            .orderBy("date")
+
+        // Collect and format daily revenue data
+        val timeSeriesDataRev = revenueByDay
             .withColumn("date", to_date(col("date")))
             .collect()
             .map(row => (row.getAs[java.sql.Date]("date").toString, row.getAs[Double]("daily_revenue")))
-            .toSeq
-        // Convert to immutable Seq explicitly and ensure dates are converted to strings
-        val dateStrings_rev = timeSeriesData_rev.map(_._1).to[scala.collection.immutable.Seq]
-        val immutableRevenue = timeSeriesData_rev.map(_._2).to[scala.collection.immutable.Seq]
 
-        plotLineChart(dateStrings, immutableRevenue, "Daily Revenue Over Time", "reports/revenue_time_series.html")
+        plotLineChart(timeSeriesDataRev.map(_._1).to[scala.collection.immutable.Seq], 
+                      timeSeriesDataRev.map(_._2).to[scala.collection.immutable.Seq], 
+                      "Daily Revenue Over Time", 
+                      "reports/revenue_time_series.html")
     }
+    def analyze(df: DataFrame): Unit = {
+        // Total Impression per day
+        val impressionsByDay = df.groupBy("date")
+            .agg(sum("total_impressions").alias("daily_impressions"))
+            .orderBy("date")
 
+        val movingAvgDF = movingAvg(impressionsByDay)
+        dailyImpression(impressionsByDay)
+        dailyRevenue(df)
+
+    }
 }
